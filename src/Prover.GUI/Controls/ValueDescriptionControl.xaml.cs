54f69d5ed3f256acd48e32289c9d6c54ba6c3772
mcquaiga <mcquaiga@gmail.com>
2016-03-09 02:07:53 -0800

    UI Refactoring

---
+++ src/Prover.GUI/Controls/ValueDescriptionControl.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+
+namespace Prover.GUI.Controls
+{
+    /// <summary>
+    /// Interaction logic for InstrumentField.xaml
+    /// </summary>
+    public partial class ValueDescriptionControl : UserControl
+    {
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Label.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register("Label", typeof(string), typeof(ValueDescriptionControl));
+
+        public string Value
+        {
+            get { return (string)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Value.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(string), typeof(ValueDescriptionControl));
+
+
+        public ValueDescriptionControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
