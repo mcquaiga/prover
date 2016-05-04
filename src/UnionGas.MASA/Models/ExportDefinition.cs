ec60b09a264fe7712ea7f4d01819f0c797c4528a
mcquaiga <mcquaiga@gmail.com>
2016-05-04 12:10:47 -0700

    updates to export definition

--- src/UnionGas.MASA/Models/ExportDefinition.cs
+++ src/UnionGas.MASA/Models/ExportDefinition.cs
@@ -8,36 +8,48 @@ namespace UnionGas.MASA.Models
 {
     public class EvcQARun
     {
-        public EvcQARun()
-        {
-
-        }
-
         public DateTime DateTime { get; set; }
         public string InstrumentType { get; set; }
-        public List<VerificationTest> VerificationTests { get; set; };
+        public List<VerificationTest> VerificationTests { get; set; }
+
+        //Pressure Info
+        public string TransducerType { get; set; }
+        public string PressureUnits { get; set; }
+        public decimal EvcBasePressure { get; set; }
+        public decimal EvcPressureRange { get; set; }
+
+        //Temperature Info
+        public string Range { get; set; }
+        public string TemperatureUnits { get; set; }
+        public decimal EvcBaseTemperature { get; set; }
+
+        //Supercompress. Info
+        public decimal SpecGr { get; set; }
+        public decimal CO2 { get; set; }
+        public decimal N2 { get; set; }
+        public string SuperTable { get; set; }
+
+        //Volume Info
+        public string PulseASelect { get; set; }
+        public string PulseBSelect { get; set; }
+        public string DriveTypeDiscriminator { get; set; }
 
         public class VerificationTest
         {
-            public VerificationTest()
-            {
-
-            }
-
             public virtual PressureDetail Pressure { get; set; }
             public virtual TemperatureDetail Temperature { get; set; }
             public virtual VolumeDetail VolumeTest { get; set; }
-            public virtual SuperFactorTest SuperFactorTest { get; set; }
+            public virtual SuperFactorDetail SuperFactorTest { get; set; }
 
             public class PressureDetail
             {
                 public decimal GasPressure { get; set; }
-                public decimal GasGauge { get; set; }
+                public decimal GaugePressure { get; set; }
                 public decimal AtmosphericGauge { get; set; }
                 public decimal ActualFactor { get; set; }
 
-                public decimal EvcPressure { get; set; }
-                public decimal EvcFactor { get; set; }
+                public decimal EvcGasPressure { get; set; }
+                public decimal EvcPressureFactor { get; set; }
                 public decimal PercentError { get; set; }
             }
 
@@ -54,29 +66,32 @@ namespace UnionGas.MASA.Models
             {
                 public int PulseACount { get; set; }
                 public int PulseBCount { get; set; }
-                public decimal AppliedInput { get; set; }
-                public string DriveTypeDiscriminator { get; set; }            
-                public decimal? UnCorrectedPercentError { get; set; }
-                public decimal? CorrectedPercentError { get; set; }
+                public decimal AppliedInput { get; set; }         
+                public decimal UnCorrectedPercentError { get; set; }
+                public decimal CorrectedPercentError { get; set; }
                 public int UncPulseCount { get; set; }
                 public int CorPulseCount { get; set; }
-                public decimal? TrueCorrected { get; set; }
-
-                public string PulseASelect { get; set; }
-
-                public string PulseBSelect { get; set; }
+                public decimal TrueCorrected { get; set; }
                 public decimal EvcCorrected { get; set; }
-
                 public decimal EvcUncorrected { get; set; }
 
                 public decimal Corrected { get; set; }
-
                 public decimal Uncorrected { get; set; }
-                public string DriveRateDescription { get; set; }
 
+                public string DriveRateDescription { get; set; }
                 public string CorrectedMultiplierDescription { get; set; }
                 public string UnCorrectedMultiplierDescription { get; set; }
+            }
 
+            public class SuperFactorDetail
+            {
+                public decimal GaugeTemp { get; set; }
+                public decimal GaugePressure { get; set; }
+
+                public decimal EVCUnsqrFactor { get; set; }
+                public decimal EvcFactor { get; set; }
+                public decimal ActualFactor { get; set; }
+                public decimal PercentError { get; set; }
             }
         }
     }
