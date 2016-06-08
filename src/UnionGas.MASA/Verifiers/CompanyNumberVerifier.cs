6f171d037f897b37c91ec0309f03141bd3b6741f
mcquaiga <mcquaiga@gmail.com>
2016-06-07 20:34:23 -0700

    Company Number verifier

--- src/UnionGas.MASA/Verifiers/CompanyNumberVerifier.cs
+++ src/UnionGas.MASA/Verifiers/CompanyNumberVerifier.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +10,41 @@ namespace UnionGas.MASA
 {
     public class CompanyNumberVerifier
     {
-        public CompanyNumberVerifier()
-        {
+        private const string VerifyEndPoint = "Verify";
+        private const string UpdateEndPoint = "Update";
 
+        public CompanyNumberVerifier(Uri serverUri, long serialNumber, long companyNumber)
+        {
+            this.ServerUri = serverUri;
+            this.SerialNumber = serialNumber;
+            this.CompanyNumber = companyNumber;
+        }
+
+        public long SerialNumber { get; set; }
+
+        public Uri ServerUri { get; private set; }
+
+        public long CompanyNumber { get; private set; }
+
+        public async Task<object> TryVerify()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(ServerUri, new StringContent(CompanyNumber.ToString()));
+                response.EnsureSuccessStatusCode();
+
+                var content = await response.Content.ReadAsStringAsync();
+
+                return content;
+            }
+        }
+
+        public async Task<bool> UpdateCompanyNumber()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(ServerUri, new )
+            } 
         }
     }
 }
