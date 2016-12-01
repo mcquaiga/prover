25100adeecf7359f2dd1d31852439afff46d1ab4
mcquaiga <mcquaiga@gmail.com>
2016-12-01 10:54:55 -0500

    remove un-used projects

--- src/Tests/Prover.Core.Tests/TachometerTests.cs
+++ src/Tests/Prover.Core.Tests/TachometerTests.cs
@@ -7,15 +7,15 @@ namespace Prover.Core.Tests
     [TestClass]
     public class TachometerTests
     {
-        [TestMethod]
-        public void TestParseTachometer()
-        {
-            const string value = @" @D0
-OK
-      12000
+//        [TestMethod]
+//        public void TestParseTachometer()
+//        {
+//            const string value = @" @D0
+//OK
+//      12000
 
-";
-            var test = TachometerCommunicator.ParseTachValue(value);
-        }
+//";
+//            var test = TachometerCommunicator.ParseTachValue(value);
+//        }
     }
 }
