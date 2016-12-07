3ad4c80d08d0a503ef78c7d21eb36df1b47bf390
mcquaiga <mcquaiga@gmail.com>
2016-12-07 07:53:12 -0500

    clean up

--- src/Prover.GUI/Screens/QAProver/TestRunViewModel.cs
+++ src/Prover.GUI/Screens/QAProver/TestRunViewModel.cs
@@ -1,29 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO.Ports;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using Akka.Util.Internal;
 using Caliburn.Micro;
-using MaterialDesignThemes.Wpf;
 using Prover.CommProtocol.Common;
-using Prover.CommProtocol.Common.IO;
 using Prover.CommProtocol.MiHoneywell;
 using Prover.Core.DriveTypes;
 using Prover.Core.Events;
-using Prover.Core.Models.Instruments;
 using Prover.Core.Settings;
 using Prover.Core.VerificationTests;
 using Prover.GUI.Common;
 using Prover.GUI.Common.Screens;
-using Prover.GUI.Dialogs;
 using Prover.GUI.Screens.QAProver.PTVerificationViews;
-using Prover.GUI.Screens.Settings;
 using ReactiveUI;
 using Splat;
-using SerialPort = System.IO.Ports.SerialPort;
 
 namespace Prover.GUI.Screens.QAProver
 {
@@ -32,42 +27,61 @@ namespace Prover.GUI.Screens.QAProver
         private const string NewQaTestViewContext = "NewTestView";
         private const string EditQaTestViewContext = "EditTestView";
 
-        private IQaRunTestManager _qaRunTestManager;
+        private IReactiveDerivedList<string> _commPort;
 
-        public ReactiveCommand<object> StartTestCommand { get; private set; }
+        private string _connectionStatusMessage;
+        private ReactiveList<SelectableInstrumentType> _instrumentTypes;
+
+        private IQaRunTestManager _qaRunTestManager;
+        private int _selectedBaudRate;
+
+        private string _selectedCommPort;
+        private string _selectedTachCommPort;
+
+        private bool _showConnectionDialog;
+
+        private string _viewContext;
 
         public TestRunViewModel(ScreenManager screenManager, IEventAggregator eventAggregator)
             : base(screenManager, eventAggregator)
         {
+            eventAggregator.Subscribe(this);
             SettingsManager.RefreshSettings();
 
             /***  Setup Instruments list  ***/
             InstrumentTypes = new ReactiveList<SelectableInstrumentType>
             {
-                ChangeTrackingEnabled = true,
+                ChangeTrackingEnabled = true
             };
 
             Instruments.GetAll().ForEach(
                 x => InstrumentTypes.Add(new SelectableInstrumentType
-                    {
-                        Instrument = x,
-                        IsSelected = x.Name == SettingsManager.SettingsInstance.LastInstrumentTypeUsed
-                    }));
-            
+                {
+                    Instrument = x,
+                    IsSelected = x.Name == SettingsManager.SettingsInstance.LastInstrumentTypeUsed
+                }));
+
             InstrumentTypes.ItemChanged
-                .Where(x => x.PropertyName == "IsSelected" && x.Sender.IsSelected)
+                .Where(x => (x.PropertyName == "IsSelected") && x.Sender.IsSelected)
                 .Select(x => x.Sender)
-                .Subscribe(x => {
+                .Subscribe(x =>
+                {
                     SettingsManager.SettingsInstance.LastInstrumentTypeUsed = x.Instrument.Name;
                     SettingsManager.Save();
                 });
 
             /***  Setup Comm Ports and Baud Rate settings ***/
             CommPort = PortsWatcherObservable().CreateCollection();
-            
-            _selectedCommPort = CommPort.Contains(SettingsManager.SettingsInstance.InstrumentCommPort) ? SettingsManager.SettingsInstance.InstrumentCommPort : string.Empty;
-            _selectedBaudRate = BaudRate.Contains(SettingsManager.SettingsInstance.InstrumentBaudRate) ? SettingsManager.SettingsInstance.InstrumentBaudRate : -1;
-            _selectedTachCommPort = TachCommPort.Contains(SettingsManager.SettingsInstance.TachCommPort) ? SettingsManager.SettingsInstance.TachCommPort : string.Empty;
+
+            _selectedCommPort = CommPort.Contains(SettingsManager.SettingsInstance.InstrumentCommPort)
+                ? SettingsManager.SettingsInstance.InstrumentCommPort
+                : string.Empty;
+            _selectedBaudRate = BaudRate.Contains(SettingsManager.SettingsInstance.InstrumentBaudRate)
+                ? SettingsManager.SettingsInstance.InstrumentBaudRate
+                : -1;
+            _selectedTachCommPort = TachCommPort.Contains(SettingsManager.SettingsInstance.TachCommPort)
+                ? SettingsManager.SettingsInstance.TachCommPort
+                : string.Empty;
 
             this.WhenAnyValue(x => x.SelectedBaudRate, x => x.SelectedCommPort, x => x.SelectedTachCommPort)
                 .Subscribe(_ =>
@@ -79,14 +93,37 @@ namespace Prover.GUI.Screens.QAProver
                 });
 
             /*** Commands ***/
-            var canStartNewTest = this.WhenAnyValue(x => x.SelectedBaudRate, x => x.SelectedCommPort, x => x.SelectedTachCommPort,
-                (baud, instrumentPort, tachPort) => 
-                    BaudRate.Contains(baud) && !string.IsNullOrEmpty(instrumentPort) && !string.IsNullOrEmpty(tachPort));
+            var canStartNewTest = this.WhenAnyValue(x => x.SelectedBaudRate, x => x.SelectedCommPort,
+                x => x.SelectedTachCommPort,
+                (baud, instrumentPort, tachPort) =>
+                    BaudRate.Contains(baud) && !string.IsNullOrEmpty(instrumentPort) &&
+                    !string.IsNullOrEmpty(tachPort));
 
             StartTestCommand = ReactiveCommand.Create(canStartNewTest);
             StartTestCommand.Subscribe(async _ => await StartNewQaTest());
 
             _viewContext = NewQaTestViewContext;
+        }
+
+        public ReactiveCommand<object> StartTestCommand { get; }
+        
+        public RotaryMeterTestViewModel MeterDisplacementItem { get; set; }
+
+        public InstrumentInfoViewModel SiteInformationItem { get; set; }
+
+        public ObservableCollection<VerificationSetViewModel> TestViews { get; set; } =
+            new ObservableCollection<VerificationSetViewModel>();
+
+        public VolumeTestViewModel VolumeInformationItem { get; set; }
+
+        public void Dispose()
+        {
+            _qaRunTestManager?.Dispose();
+        }
+
+        public void Handle(ConnectionStatusEvent message)
+        {
+            ConnectionStatusMessage = $"Attempt {message.ConnectionStatus} of {message.MaxAttempts}...";
         }
 
         private IObservable<string> PortsWatcherObservable()
@@ -99,70 +136,9 @@ namespace Prover.GUI.Screens.QAProver
                         _ =>
                         {
                             if (!_commPort.SequenceEqual(SerialPort.GetPortNames()))
-                            {
                                 SerialPort.GetPortNames().ForEach(observer.OnNext);
-                            }
                         });
             });
-        }
-
-        private string _viewContext;
-        public string ViewContext
-        {
-            get { return _viewContext; }
-            set { this.RaiseAndSetIfChanged(ref _viewContext, value); }
-        }
-
-        public InstrumentType SelectedInstrument => InstrumentTypes?.FirstOrDefault(i => i.IsSelected)?.Instrument;
-        private ReactiveList<SelectableInstrumentType> _instrumentTypes;
-        public ReactiveList<SelectableInstrumentType> InstrumentTypes
-        {
-            get { return _instrumentTypes; }
-            set { this.RaiseAndSetIfChanged(ref _instrumentTypes, value); }
-        }
-
-        private IReactiveDerivedList<string> _commPort;
-        public IReactiveDerivedList<string> CommPort
-        {
-            get { return _commPort; }
-            set { this.RaiseAndSetIfChanged(ref _commPort, value); }
-        }
-
-        private string _selectedCommPort;
-        public string SelectedCommPort
-        {
-            get { return _selectedCommPort; }
-            set { this.RaiseAndSetIfChanged(ref _selectedCommPort, value); }
-        }
-
-        public List<string> TachCommPort => SerialPort.GetPortNames().ToList();
-        private string _selectedTachCommPort;
-        public string SelectedTachCommPort
-        {
-            get { return _selectedTachCommPort; }
-            set { this.RaiseAndSetIfChanged(ref _selectedTachCommPort, value); }
-        }
-
-        public List<int> BaudRate => CommProtocol.Common.IO.SerialPort.BaudRates;
-        private int _selectedBaudRate;
-        public int SelectedBaudRate
-        {
-            get { return _selectedBaudRate; }
-            set { this.RaiseAndSetIfChanged(ref _selectedBaudRate, value); }
-        }
-
-        private bool _showConnectionDialog;
-        public bool ShowConnectionDialog
-        {
-            get { return _showConnectionDialog; }
-            set { this.RaiseAndSetIfChanged(ref _showConnectionDialog, value); }
-        }
-
-        private string _connectionStatusMessage;
-        public string ConnectionStatusMessage
-        {
-            get { return _connectionStatusMessage; }
-            set { this.RaiseAndSetIfChanged(ref _connectionStatusMessage, value); }
         }
 
         public async Task CancelCommand()
@@ -172,10 +148,8 @@ namespace Prover.GUI.Screens.QAProver
 
         public async Task StartNewQaTest()
         {
-            if (SelectedInstrument != null) 
+            if (SelectedInstrument != null)
             {
-                
-
                 _qaRunTestManager = Locator.Current.GetService<IQaRunTestManager>();
 
                 SettingsManager.SettingsInstance.LastInstrumentTypeUsed = SelectedInstrument.Name;
@@ -202,8 +176,9 @@ namespace Prover.GUI.Screens.QAProver
                         }
 
                         if (_qaRunTestManager.Instrument.VolumeTest?.DriveType is RotaryDrive)
-                            MeterDisplacementItem = 
-                                new RotaryMeterTestViewModel((RotaryDrive)_qaRunTestManager.Instrument.VolumeTest.DriveType);
+                            MeterDisplacementItem =
+                                new RotaryMeterTestViewModel(
+                                    (RotaryDrive) _qaRunTestManager.Instrument.VolumeTest.DriveType);
                     });
 
                     ViewContext = EditQaTestViewContext;
@@ -216,29 +191,64 @@ namespace Prover.GUI.Screens.QAProver
             }
         }
 
-        public RotaryMeterTestViewModel MeterDisplacementItem { get; set; }
+        public string ViewContext
+        {
+            get { return _viewContext; }
+            set { this.RaiseAndSetIfChanged(ref _viewContext, value); }
+        }
 
-        public InstrumentInfoViewModel SiteInformationItem { get; set; }
+        public InstrumentType SelectedInstrument => InstrumentTypes?.FirstOrDefault(i => i.IsSelected)?.Instrument;
 
-        public ObservableCollection<VerificationSetViewModel> TestViews { get; set; } =
-            new ObservableCollection<VerificationSetViewModel>();
+        public ReactiveList<SelectableInstrumentType> InstrumentTypes
+        {
+            get { return _instrumentTypes; }
+            set { this.RaiseAndSetIfChanged(ref _instrumentTypes, value); }
+        }
 
-        public VolumeTestViewModel VolumeInformationItem { get; set; }
+        public IReactiveDerivedList<string> CommPort
+        {
+            get { return _commPort; }
+            set { this.RaiseAndSetIfChanged(ref _commPort, value); }
+        }
+
+        public string SelectedCommPort
+        {
+            get { return _selectedCommPort; }
+            set { this.RaiseAndSetIfChanged(ref _selectedCommPort, value); }
+        }
+
+        public List<string> TachCommPort => SerialPort.GetPortNames().ToList();
+
+        public string SelectedTachCommPort
+        {
+            get { return _selectedTachCommPort; }
+            set { this.RaiseAndSetIfChanged(ref _selectedTachCommPort, value); }
+        }
+
+        public List<int> BaudRate => CommProtocol.Common.IO.SerialPort.BaudRates;
+
+        public int SelectedBaudRate
+        {
+            get { return _selectedBaudRate; }
+            set { this.RaiseAndSetIfChanged(ref _selectedBaudRate, value); }
+        }
+
+        public bool ShowConnectionDialog
+        {
+            get { return _showConnectionDialog; }
+            set { this.RaiseAndSetIfChanged(ref _showConnectionDialog, value); }
+        }
+
+        public string ConnectionStatusMessage
+        {
+            get { return _connectionStatusMessage; }
+            set { this.RaiseAndSetIfChanged(ref _connectionStatusMessage, value); }
+        }
 
         public class SelectableInstrumentType
         {
             public InstrumentType Instrument { get; set; }
             public bool IsSelected { get; set; }
-        }
-
-        public void Dispose()
-        {
-            _qaRunTestManager?.Dispose();
-        }
-
-        public void Handle(ConnectionStatusEvent message)
-        {
-            ConnectionStatusMessage = $"Attempt {message.ConnectionStatus} of {message.MaxAttempts}...";
         }
     }
 }
