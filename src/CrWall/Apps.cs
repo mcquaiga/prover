ba7029634d1c6578e2ee3f489c06b8c5919f414b
mcquaiga <mcquaiga@gmail.com>
2016-12-14 10:12:24 -0500

    client manager app

--- src/CrWall/Apps.cs
+++ src/CrWall/Apps.cs
@@ -3,23 +3,38 @@ using System.Xaml;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using Caliburn.Micro;
+using CrWall.Screens.Clients;
 using Prover.GUI.Common;
 using Prover.GUI.Common.Screens.MainMenu;
 using Action = System.Action;
 
 namespace CrWall
 {
-    public class CertificateManagerApp : AppMainMenuBase
+    //public class CertificateManagerApp : AppMainMenuBase
+    //{
+    //    public CertificateManagerApp(ScreenManager screenManager, IEventAggregator eventAggregator)
+    //        : base(screenManager, eventAggregator)
+    //    {
+    //    }
+
+    //    public override ImageSource IconSource
+    //        => new BitmapImage(new Uri("pack://application:,,,/CrWall;component/Resources/dd.png"));
+
+    //    public override string AppTitle => "Create Certificate";
+    //    public override Action ClickAction => async () => await ScreenManager.ChangeScreen<CertificateCreatorViewModel>();
+    //}
+
+    public class ClientManagerApp : AppMainMenuBase
     {
-        public CertificateManagerApp(ScreenManager screenManager, IEventAggregator eventAggregator)
+        public ClientManagerApp(ScreenManager screenManager, IEventAggregator eventAggregator)
             : base(screenManager, eventAggregator)
         {
         }
 
         public override ImageSource IconSource
-            => new BitmapImage(new Uri("pack://application:,,,/UnionGas;component/Resources/dd.png"));
+            => new BitmapImage(new Uri("pack://application:,,,/CrWall;component/Resources/group.png"));
 
-        public override string AppTitle => "Create Certificate";
-        public override Action ClickAction => async () => await ScreenManager.ChangeScreen<CertificateCreatorViewModel>();
+        public override string AppTitle => "Manage Clients";
+        public override Action ClickAction => async () => await ScreenManager.ChangeScreen<ClientManagerViewModel>();
     }
 }
---
+++ src/CrWall/Screens/Clients/ClientManagerViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using Prover.GUI.Common;
+using Prover.GUI.Common.Screens;
+
+namespace CrWall.Screens.Clients
+{
+    public class ClientManagerViewModel : ViewModelBase
+    {
+        public ClientManagerViewModel(ScreenManager screenManager, IEventAggregator eventAggregator) : base(screenManager, eventAggregator)
+        {
+
+        }
+    }
+}
