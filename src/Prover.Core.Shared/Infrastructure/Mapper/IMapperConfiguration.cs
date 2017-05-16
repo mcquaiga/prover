ed556b18abcd087cb4cddba903a1b518e748b38e
mcquaiga <mcquaiga@gmail.com>
2017-05-15 23:33:25 -0700

    fix dependencies

--- src/Prover.Core.Shared/Infrastructure/Mapper/IMapperConfiguration.cs
+++ src/Prover.Core.Shared/Infrastructure/Mapper/IMapperConfiguration.cs
@@ -1,26 +1,22 @@
-﻿#region
+﻿//using System;
+//using AutoMapper;
 
-using System;
-using AutoMapper;
+//namespace Prover.Core.Shared.Infrastructure.Mapper
+//{
+//    /// <summary>
+//    ///     Mapper configuration registrar interface
+//    /// </summary>
+//    public interface IMapperConfiguration
+//    {
+//        /// <summary>
+//        ///     Get configuration
+//        /// </summary>
+//        /// <returns>Mapper configuration action</returns>
+//        Action<IMapperConfigurationExpression> GetConfiguration();
 
-#endregion
-
-namespace Prover.Core.Shared.Infrastructure.Mapper
-{
-    /// <summary>
-    ///     Mapper configuration registrar interface
-    /// </summary>
-    public interface IMapperConfiguration
-    {
-        /// <summary>
-        ///     Get configuration
-        /// </summary>
-        /// <returns>Mapper configuration action</returns>
-        Action<IMapperConfigurationExpression> GetConfiguration();
-
-        /// <summary>
-        ///     Order of this mapper implementation
-        /// </summary>
-        int Order { get; }
-    }
-}
+//        /// <summary>
+//        ///     Order of this mapper implementation
+//        /// </summary>
+//        int Order { get; }
+//    }
+//}
