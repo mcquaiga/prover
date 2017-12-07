43bd26600a65b1ceb4b6464a41136d71abeb0758
mcquaiga <mcquaiga@gmail.com>
2017-12-07 11:08:56 -0500

    checkpoint

---
+++ src/Prover.Core/VerificationTests/TestActions/PreTestActions/TocItemUpdater.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+using Prover.CommProtocol.Common;
+using Prover.CommProtocol.Common.Items;
+using Prover.Core.Models.Instruments;
+
+namespace Prover.Core.VerificationTests.TestActions.PreTestActions
+{
+    public class TocItemUpdater : IPreTestAction
+    {
+        private IEnumerable<ItemValue> _itemResetValues = new List<ItemValue>();
+
+        public TocItemUpdater()
+        {
+            _itemResetValues = new List<ItemValue>()
+            {
+                new ItemValue(new ItemMetadata() { Number = 865 }, "")
+            };
+        }
+
+        public async Task Execute(EvcCommunicationClient commClient, Instrument instrument, Subject<string> statusUpdates = null)
+        {
+            await commClient.SetItemValue()
+        }
+    }
+}
