348e87cf8dcdfb3c1528e22508e0c7692e79db98
mcquaiga <mcquaiga@gmail.com>
2018-11-27 08:54:35 -0500

    Round energy values to floor

--- src/Prover.Core/Models/Instruments/DriveTypes/MechanicalDrive.cs
+++ src/Prover.Core/Models/Instruments/DriveTypes/MechanicalDrive.cs
@@ -64,19 +64,19 @@ namespace Prover.Core.Models.Instruments.DriveTypes
                 switch (EnergyUnits)
                 {
                     case Therms:
-                        return Math.Round(energyValue * _instrument.VolumeTest.EvcCorrected.Value / 100000);
+                        return Math.Floor(energyValue * _instrument.VolumeTest.EvcCorrected.Value / 100000);
 
                     case Dktherms:
-                        return Math.Round(energyValue * _instrument.VolumeTest.EvcCorrected.Value / 1000000) ;
+                        return Math.Floor(energyValue * _instrument.VolumeTest.EvcCorrected.Value / 1000000) ;
 
                     case GigaJoules:
-                        return Math.Round(energyValue * _instrument.VolumeTest.EvcCorrected.Value / 1000000);
+                        return Math.Floor(energyValue * _instrument.VolumeTest.EvcCorrected.Value / 1000000);
 
                     case MegaJoules:
-                        return Math.Round(energyValue * _instrument.VolumeTest.EvcCorrected.Value / 1000);
+                        return Math.Floor(energyValue * _instrument.VolumeTest.EvcCorrected.Value / 1000);
 
                     case KiloCals:
-                        return Math.Round(energyValue * 0.0283168m * _instrument.VolumeTest.EvcCorrected.Value);
+                        return Math.Floor(energyValue * 0.0283168m * _instrument.VolumeTest.EvcCorrected.Value);
 
                     default:
                         throw new Exception(string.Format("Energy units not supported: {0}", EnergyUnits));
