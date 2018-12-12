e909eace9b671879a6730ee31814f562309dccfb
mcquaiga <mcquaiga@gmail.com>
2018-12-12 08:10:27 -0500

    checkpoint

---
+++ src/UnionGas.MASA/DCRWebServiceCommunicator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnionGas.MASA.DCRWebService;
+
+namespace UnionGas.MASA
+{
+    public class DCRWebServiceCommunicator
+    {
+        private readonly DCRWebServiceSoap _dcrWebService;
+
+        public DCRWebServiceCommunicator(DCRWebServiceSoap dcrWebService)
+        {
+            _dcrWebService = dcrWebService;
+        }
+
+        public async Task<IList<MeterDTO>> GetOutstandingMeterTestsByJobNumber(int jobNumber)
+        {
+            var request = new GetMeterListByJobNumberRequest(new GetMeterListByJobNumberRequestBody(jobNumber));
+
+            var response = await _dcrWebService.GetMeterListByJobNumberAsync(request);
+
+            return response.Body.GetMeterListByJobNumberResult.ToList();
+        }
+    }
+}
