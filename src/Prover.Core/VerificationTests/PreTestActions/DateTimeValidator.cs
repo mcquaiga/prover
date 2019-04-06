59bc2a34d3acf1c2e9a7967def86e62d17091b80
Adam McQuaig <mcquaiga@gmail.com>
2019-04-06 09:31:51 -0400

    fixing merge errors

--- src/Prover.Core/VerificationTests/PreTestActions/DateTimeValidator.cs
+++ src/Prover.Core/VerificationTests/PreTestActions/DateTimeValidator.cs
@@ -18,9 +18,11 @@ namespace Prover.Core.VerificationTests.PreTestActions
 
             if (!instrument.GetDateTime().Equals(dtNow))
             {
+                await commClient.Connect(new System.Threading.CancellationToken());
                 _log.Info("Resetting instrument date/time...");
                 await commClient.SetItemValue(203, instrument.GetTimeFormatted(dtNow));
                 await commClient.SetItemValue(204, instrument.GetDateFormatted(dtNow));
+                await commClient.Disconnect();
             }
         }
     }
