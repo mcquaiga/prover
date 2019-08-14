8d75e7a8c347677af65ff278ff0d7fb2fdd9b19d
Adam McQuaig <mcquaiga@gmail.com>
2019-08-14 10:07:18 -0400

    adding logging

--- src/Prover.Core/VerificationTests/VolumeVerification/AutoVolumeTestManager.cs
+++ src/Prover.Core/VerificationTests/VolumeVerification/AutoVolumeTestManager.cs
@@ -238,14 +238,17 @@
                                pulsesWaiting += (int)i.NumericValue;
                            }
 
-                           Status.OnNext($"Waiting for residual pulses...{Environment.NewLine} {pulsesWaiting} total pulses remaining");
+                           Log.Debug($"Waiting for residual pulses...{Environment.NewLine} {pulsesWaiting} total pulses remaining.");
+                           Status.OnNext($"Waiting for residual pulses...{Environment.NewLine} {pulsesWaiting} total pulses remaining.");
                            if (pulsesWaiting > 0 && lastPulsesWaiting != pulsesWaiting)
                            {
+                               Log.Debug($"Pulses waiting...");
                                await commClient.Disconnect();
                                lastPulsesWaiting = pulsesWaiting;
                            }
                            else
                            {
+                               Log.Debug($"No Pulses waiting!");
                                keepWaiting = false;
                            }
                        }))
