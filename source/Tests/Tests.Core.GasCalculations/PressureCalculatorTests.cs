f9d74c2a3543e5cf79edfeebc4c8b44ba75356f7
Adam McQuaig <mcquaiga@gmail.com>
2020-03-02 08:18:38 -0800

    Update Unit tests

--- source/Tests/Tests.Core.GasCalculations/PressureCalculatorTests.cs
+++ source/Tests/Tests.Core.GasCalculations/PressureCalculatorTests.cs
@@ -1,86 +1,82 @@
-﻿//using Core.GasCalculations;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using Moq;
-//using Shared;
+﻿using Core.GasCalculations;
+using Devices.Core.Items.ItemGroups;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Prover.Shared;
 
-//namespace Tests.Core.GasCalculations
-//{
-//    [TestClass()]
-//    public class PressureCalculatorTests
-//    {
-//        [TestInitialize]
-//        public void Initialize()
-//        {
-//            //PressureValues = new Mock<PressureItems>();
+namespace Tests.Core.GasCalculations
+{
+    [TestClass()]
+    public class PressureCalculatorTests
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            //PressureValues = new Mock<PressureItems>();
 
-//            //PressureValues.Setup(p => p.AtmosphericPressure).Returns(14.7300m);
-//            //PressureValues.Setup(p => p.Base).Returns(14.7300m);
-//            //PressureValues.Setup(p => p.Factor).Returns(6.4311m);
-//            //PressureValues.Setup(p => p.GasPressure).Returns(80.0m);
-//            //PressureValues.Setup(p => p.Range).Returns(100);
-//            //PressureValues.Setup(p => p.TransducerType).Returns(PressureTransducerType.Gauge);
-//            //PressureValues.Setup(p => p.UnitType).Returns(PressureUnitType.PSIG);
-//            //PressureValues.Setup(p => p.UnsqrFactor).Returns(1.0076m);
-//        }
+            //PressureValues.Setup(p => p.AtmosphericPressure).Returns(14.7300m);
+            //PressureValues.Setup(p => p.Base).Returns(14.7300m);
+            //PressureValues.Setup(p => p.Factor).Returns(6.4311m);
+            //PressureValues.Setup(p => p.GasPressure).Returns(80.0m);
+            //PressureValues.Setup(p => p.Range).Returns(100);
+            //PressureValues.Setup(p => p.TransducerType).Returns(PressureTransducerType.Gauge);
+            //PressureValues.Setup(p => p.UnitType).Returns(PressureUnitType.PSIG);
+            //PressureValues.Setup(p => p.UnsqrFactor).Returns(1.0076m);
+        }
 
-//        public Mock<PressureItems> PressureValues
-//        { get; set; }
+        public Mock<PressureItems> PressureValues
+        { get; set; }
 
 
-//        [TestMethod()]
-//        public void PressureCalculatorTest()
-//        {
-//            Assert.IsTrue(true);
-//        }
+        [TestMethod()]
+        public void PressureCalculatorTest()
+        {
+            Assert.IsTrue(true);
+        }
 
-//        [TestMethod()]
-//        public void GasPressureGaugeTransducerTest()
-//        {
-//            //var gauge = 80.0m;
-//            //var atm = 14.73m;
-//            //var expected = gauge + atm;
+        [TestMethod()]
+        public void GasPressureGaugeTransducerTest()
+        {
+            var gauge = 80.0m;
+            var atm = 14.73m;
+            var expected = gauge + atm;
 
-//            //var mock = new Mock<PressureItems>();
-//            //mock.Setup(p => p.TransducerType).Returns(PressureTransducerType.Gauge);
+            var calc = new PressureCalculator(PressureUnitType.PSIG, PressureTransducerType.Gauge, 60.0m, gauge, atm);
+            Assert.IsTrue(calc.GasPressure == expected);
+        }
 
-//            //var calc = mock.Object.GetCalculator(gauge, atm);
-//            //Assert.IsTrue(calc.GasPressure == expected);
-//        }
+        [TestMethod()]
+        public void GasPressureAbsoluteTransducerTest()
+        {
+            var gauge = 80.0m;
+            var atm = 14.73m;
+            var expected = gauge;
 
-//        [TestMethod()]
-//        public void GasPressureAbsoluteTransducerTest()
-//        {
-//            var gauge = 80.0m;
-//            var atm = 14.73m;
-//            var expected = gauge;
+            var calc = new PressureCalculator(PressureUnitType.PSIG, PressureTransducerType.Absolute, 60.0m, gauge, atm);
+       
+            Assert.IsTrue(calc.GasPressure == expected);
+        }
 
-//            var mock = new Mock<PressureItems>();
-//            mock.Setup(p => p.TransducerType).Returns(PressureTransducerType.Absolute);
+        [TestMethod()]
+        public void CalculateFactorTest()
+        {
+            var gauge = 80.0m;
+            var atm = 14.73m;
+            var expected = 6.4311m;
+            var calc = new PressureCalculator(PressureUnitType.PSIG, PressureTransducerType.Gauge, 14.7300m, gauge, atm);
+            Assert.IsTrue(calc.CalculateFactor() == expected);
+        }
 
-//            var calc = mock.Object.GetCalculator(gauge, atm);
-//            Assert.IsTrue(calc.GasPressure == expected);
-//        }
+        [TestMethod()]
+        public void GetGaugePressureTest()
+        {
+            var range = 100;
+            var percent = .80m;
+            var expected = 80;
 
-//        [TestMethod()]
-//        public void CalculateFactorTest()
-//        {
-//            var gauge = 80.0m;
-//            var atm = 14.73m;
-//            var expected = 6.4311m;
-//            var calc = PressureValues.Object.GetCalculator(gauge, atm);
-//            Assert.IsTrue(calc.CalculateFactor() == expected);
-//        }
+            var gauge = PressureCalculator.GetGaugePressure(range, percent);
 
-//        [TestMethod()]
-//        public void GetGaugePressureTest()
-//        {
-//            var range = 100;
-//            var percent = .80m;
-//            var expected = 80;
-
-//            var gauge = PressureCalculator.GetGaugePressure(range, percent);
-
-//            Assert.IsTrue(gauge == expected);
-//        }
-//    }
-//}
+            Assert.IsTrue(gauge == expected);
+        }
+    }
+}
