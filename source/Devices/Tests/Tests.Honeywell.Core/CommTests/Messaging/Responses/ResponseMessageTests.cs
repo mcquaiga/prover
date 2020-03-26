e1fe1c092ae57b94c562d4212073c575a3b5f858
Adam McQuaig <mcquaiga@gmail.com>
2020-03-25 17:17:10 -0700

    checkpoint

---
+++ source/Devices/Tests/Tests.Honeywell.Core/CommTests/Messaging/Responses/ResponseMessageTests.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+using Devices.Communications;
+using Devices.Communications.Interfaces;
+using Devices.Communications.IO;
+using Devices.Honeywell.Comm;
+using Devices.Honeywell.Comm.Exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Prover.Shared.IO;
+
+namespace Tests.Honeywell.Comm.Messaging.Responses
+{
+    [TestClass]
+    public class ResponseMessageTests : BaseHoneywellTest
+    {
+        [TestInitialize]
+        public async Task Init()
+        {
+          
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(HoneywellResponseException))]
+        public async Task FramingError()
+        {
+            await TryConnection(ResponseCode.FramingError);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(SignOnErrorException))]
+        public async Task SignOnErrorThrown()
+        {
+            await TryConnection(ResponseCode.SignOnError);
+        }
+
+        private async Task<ICommunicationsClient> TryConnection(ResponseCode response)
+        {
+            var commMock = new Mock<ICommPort>();
+            var incoming = new Subject<string>();
+            var outgoing = new Subject<string>();
+
+            SetupComm(commMock, incoming, outgoing);
+            
+            commMock.Setup(c => c.Send(It.IsAny<string>()))
+             .Returns(Task.FromResult(default(object)));
+
+            commMock.Setup(c => c.Send(ControlCharacters.ENQ.ToString()))
+               .Callback(() => incoming.OnNext(ControlCharacters.ACK.ToString()));
+
+            commMock.Setup(c => c.Send(It.Is<string>(s => s.Length > 1)))
+                .Callback(() => incoming.OnNext(Messages.Incoming.GetResponse(response)));
+
+            var client = CommunicationsClientFactory.CreateClient(Device, commMock.Object); 
+            await client.ConnectAsync(0, TimeSpan.FromMilliseconds(50));
+            return client;
+        }
+    }
+}
---
+++ source/Prover.Externals.MccDaq/InputChannelDataAcqBoard.cs
@@ -0,0 +1,32 @@
+﻿using MccDaq;
+using Microsoft.Extensions.Logging;
+using Prover.Shared.Interfaces;
+
+namespace Prover.Externals.MccDaq
+{
+    public class InputChannelDataAcqBoard : DataAcqBoardBase, IInputChannel
+    {
+        private const int OffSignalValue = 255;
+
+        public InputChannelDataAcqBoard(ILogger logger, MccBoard boardNumber, DigitalPortType channelType,
+            int channelNumber)
+            : base(logger, boardNumber, channelType, channelNumber)
+        {
+        }
+
+        public int GetValue()
+        {
+            UlStatErrorInfo = Board.DIn(ChannelType, out short value);
+
+            if (UlStatErrorInfo.Value == ErrorInfo.ErrorCode.NoErrors)
+            {
+                return value;
+            }
+
+            if (UlStatErrorInfo.Value != ErrorInfo.ErrorCode.BadBoard)
+                Log.LogWarning("DAQ Input error: {0} - {1}", UlStatErrorInfo.Message, UlStatErrorInfo.Value);
+
+            return OffSignalValue;
+        }
+    }
+}
