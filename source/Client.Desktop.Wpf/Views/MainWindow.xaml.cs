10824b816b2b26dda387e892ca4bf336a28cccab
Adam McQuaig <mcquaiga@gmail.com>
2020-04-12 20:19:04 -0700

    checkpoint

--- source/Client.Desktop.Wpf/Views/MainWindow.xaml.cs
+++ source/Client.Desktop.Wpf/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using ReactiveUI;
+﻿using System;
+using ReactiveUI;
 using System.Reactive;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
@@ -22,8 +23,8 @@ namespace Client.Desktop.Wpf.Views
                 this.OneWayBind(ViewModel, x => x.ScreenManager.Router, x => x.RoutedViewHost.Router).DisposeWith(d);
                 this.OneWayBind(ViewModel, x => x.ToolbarItems, x => x.ToolbarItemsControl.ItemsSource).DisposeWith(d);
                 this.OneWayBind(ViewModel, x => x.ScreenManager.ToolbarItems, x => x.ToolbarActionItemsControl.ItemsSource).DisposeWith(d);
-                
-                //this.OneWayBind(ViewModel, vm => vm.MessageQueue, x => x.NotificationSnackBar.MessageQueue).DisposeWith(d);
+
+                this.Bind(ViewModel, vm => vm.MessageQueue, x => x.NotificationSnackBar.MessageQueue).DisposeWith(d);
 
                 this.BindCommand(ViewModel, x => x.NavigateBack, x => x.GoBackButton).DisposeWith(d);
                 this.BindCommand(ViewModel, x => x.NavigateHome, x => x.GoHomeButton).DisposeWith(d);
@@ -31,26 +32,28 @@ namespace Client.Desktop.Wpf.Views
                 
                 //this.BindCommand(ViewModel, x => x., x => x.GoHomeButton).DisposeWith(d);
                 //this.BindCommand(ViewModel, x => x.OpenDialog, x => x.ShowDialogButton).DisposeWith(d);
-
+                
             });
-
             RegisterNotificationInteractions();
+           
         }
 
         private void RegisterNotificationInteractions()
         {
             NotificationInteractions.SnackBarMessage.RegisterHandler(message =>
             {
-                //NotificationSnackBar.MessageQueue.Enqueue(message.Input);
+                ViewModel.MessageQueue.Enqueue(message.Input);
+
+                //NotificationSnackBar.Message.Content = message.Input;
                 //NotificationSnackBar.IsActive = true;
 
                 //Observable.Timer(TimeSpan.FromSeconds(2))
-                //    .ObserveOn(RxApp.MainThreadScheduler)
-                //    .Subscribe(_ =>
-                //    {
-                //        NotificationSnackBar.IsActive = false;
-                //        NotificationSnackBar.Message.Content = string.Empty;
-                //    });
+                //          .ObserveOn(RxApp.MainThreadScheduler)
+                //          .Subscribe(_ =>
+                //          {
+                //              NotificationSnackBar.IsActive = false;
+                //              NotificationSnackBar.Message.Content = string.Empty;
+                //          });
 
                 message.SetOutput(Unit.Default);
             });
