f0edaf240e52f4349a3f12e6bf7026911089a553
Adam McQuaig <mcquaiga@gmail.com>
2020-05-26 22:16:16 -0700

    DevOps

--- source/Prover.Shared/ProverApp.cs
+++ source/Prover.Shared/ProverApp.cs
@@ -1,15 +1,29 @@
 using System;
 using System.Diagnostics;
 using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Prover.Shared {
 	public static class ProverApp {
+		static ProverApp() {
+			VersionNumber = ProverApp.GetVersion().ToString(3);
+		}
+
+		public static void Initialize(IHostEnvironment host) {
+			AppEnvironment = host.EnvironmentName;
+		}
+
+		public static void Initialize(IServiceProvider provider) {
+			Initialize(provider.GetService<IHostEnvironment>());
+		}
+
 		public static string GetVersionNumber() {
 			return GetFileVersionInfo().FileVersion;
 		}
 
 		public static FileVersionInfo GetFileVersionInfo() {
-			var assembly = Assembly.GetExecutingAssembly();
+			var assembly = Assembly.GetEntryAssembly();
 			var fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
 			return fileVersionInfo;
 		}
@@ -17,5 +31,9 @@ namespace Prover.Shared {
 		public static Version GetVersion() {
 			return Version.Parse(GetFileVersionInfo().FileVersion);
 		}
+
+		public static string Title => "EVC Prover" + $" - v{VersionNumber}" + $" - {AppEnvironment}";
+		public static string VersionNumber { get; private set; }//=> ProverApp.GetVersionNumber();
+		public static string AppEnvironment { get; set; }
 	}
 }
