9a70f04a2aad8edd3d54a3c90028367fc7a8fb9d
Adam McQuaig <mcquaiga@gmail.com>
2020-10-21 10:09:12 -0700

    checkpoint

---
+++ src/UnionGas.MASA/Validators/BarCodeValidator.cs
@@ -0,0 +1,20 @@
+﻿using Prover.CommProtocol.Common;
+using Prover.Core.Models.Instruments;
+using Prover.Core.VerificationTests.TestActions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UnionGas.MASA.Validators {
+	public class BarCodeValidator : IEvcDeviceValidationAction {
+		public VerificationStep VerificationStep => throw new NotImplementedException();
+
+		public Task Execute(EvcCommunicationClient commClient, Instrument instrument, CancellationToken ct = default, Subject<string> statusUpdates = null) {
+			throw new NotImplementedException();
+		}
+	}
+}
