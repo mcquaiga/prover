23d972d91503f570b39c7f09cfe7f9efff35d137
Adam McQuaig <mcquaiga@gmail.com>
2021-01-19 06:47:47 -0500

    fix cross thread access error on item validation popup

--- src/Prover.GUI/Screens/Modules/ClientManager/Screens/ItemValidation/ItemValidationViewModel.cs
+++ src/Prover.GUI/Screens/Modules/ClientManager/Screens/ItemValidation/ItemValidationViewModel.cs
@@ -1,59 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Caliburn.Micro;
+﻿using Caliburn.Micro;
 using Prover.CommProtocol.Common.Items;
 using Prover.Core.Modules.Clients.VerificationTestActions;
 using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 
-namespace Prover.GUI.Screens.Modules.ClientManager.Screens.ItemValidation
-{
-    public class ItemValidationViewModel : ViewModelBase, IHandleInvalidItemVerification
-    {
-        public ItemValidationViewModel(ScreenManager screenManager, IEventAggregator eventAggregator)
-            : base(screenManager, eventAggregator)
-        {
-            SkipCommand = ReactiveCommand.Create(() => { TryClose(false); });
+namespace Prover.GUI.Screens.Modules.ClientManager.Screens.ItemValidation {
+	public class ItemValidationViewModel : ViewModelBase, IHandleInvalidItemVerification {
+		public ItemValidationViewModel(ScreenManager screenManager, IEventAggregator eventAggregator)
+			: base(screenManager, eventAggregator) {
+			SkipCommand = ReactiveCommand.Create(() => { TryClose(false); });
 
-            UpdateCommand = ReactiveCommand.Create(() => { TryClose(true); });
-        }
+			UpdateCommand = ReactiveCommand.Create(() => { TryClose(true); });
+		}
 
-        private Dictionary<ItemMetadata, Tuple<ItemValue, ItemValue>> _invalidItems;
+		private Dictionary<ItemMetadata, Tuple<ItemValue, ItemValue>> _invalidItems;
 
-        public Dictionary<ItemMetadata, Tuple<ItemValue, ItemValue>> InvalidItems
-        {
-            get => _invalidItems;
-            set => this.RaiseAndSetIfChanged(ref _invalidItems, value);
-        }
+		public Dictionary<ItemMetadata, Tuple<ItemValue, ItemValue>> InvalidItems
+		{
+			get => _invalidItems;
+			set => this.RaiseAndSetIfChanged(ref _invalidItems, value);
+		}
 
-        #region Commands
+		#region Commands
 
-        private ReactiveCommand _skipCommand;
+		private ReactiveCommand _skipCommand;
 
-        public ReactiveCommand SkipCommand
-        {
-            get => _skipCommand;
-            set => this.RaiseAndSetIfChanged(ref _skipCommand, value);
-        }
+		public ReactiveCommand SkipCommand
+		{
+			get => _skipCommand;
+			set => this.RaiseAndSetIfChanged(ref _skipCommand, value);
+		}
 
-        private ReactiveCommand _updateCommand;
+		private ReactiveCommand _updateCommand;
 
-        public ReactiveCommand UpdateCommand
-        {
-            get => _updateCommand;
-            set => this.RaiseAndSetIfChanged(ref _updateCommand, value);
-        }
+		public ReactiveCommand UpdateCommand
+		{
+			get => _updateCommand;
+			set => this.RaiseAndSetIfChanged(ref _updateCommand, value);
+		}
 
-        public bool ShouldInvalidItemsBeChanged(Dictionary<ItemMetadata, Tuple<ItemValue, ItemValue>> invalidItems)
-        {
-            if (invalidItems == null || !invalidItems.Any()) return false;
-            InvalidItems = invalidItems.OrderBy(i => i.Key.Number).ToDictionary(x => x.Key, y => y.Value);
+		public bool ShouldInvalidItemsBeChanged(Dictionary<ItemMetadata, Tuple<ItemValue, ItemValue>> invalidItems) {
+			if (invalidItems == null || !invalidItems.Any())
+				return false;
+			InvalidItems = invalidItems.OrderBy(i => i.Key.Number).ToDictionary(x => x.Key, y => y.Value);
 
-            //show dialog
-            var result = ScreenManager.ShowDialog(this);
-            return result.HasValue && result.Value;
-        }
+			//show dialog
+			bool? result = false;
 
-        #endregion
-    }
+			Application.Current.Dispatcher.Invoke(delegate {
+				result = ScreenManager.ShowDialog(this);
+			});
+
+			return result.HasValue && result.Value;
+		}
+
+		#endregion
+	}
 }
