c0e2a62fc8948c763412e8cb27009f63ccd3b8f7
Adam McQuaig <mcquaiga@gmail.com>
2021-02-10 13:50:44 -0500

    add toolbar notification opn user logging in progress

--- src/UnionGas.MASA/Screens/Toolbars/UserLoggedInEvent.cs
+++ src/UnionGas.MASA/Screens/Toolbars/UserLoggedInEvent.cs
@@ -1,18 +1,19 @@
-﻿namespace UnionGas.MASA.Screens.Toolbars
-{
-    public class UserLoggedInEvent
-    {
-        public LogInState LoginStatus { get; }
+﻿namespace UnionGas.MASA.Screens.Toolbars {
+	public class UserLoggedInEvent {
+		public LogInState LoginStatus { get; }
 
-        public enum LogInState
-        {
-            LoggedIn,
-            LoggedOut
-        }
+		public enum LogInState {
+			LoggedIn,
+			LoggedOut,
+			InProgress
+		}
 
-        public UserLoggedInEvent(LogInState loginStatus)
-        {
-            LoginStatus = loginStatus;
-        }
-    }
+		public UserLoggedInEvent(LogInState loginStatus) {
+			LoginStatus = loginStatus;
+		}
+
+		public static UserLoggedInEvent Raise(LogInState state) {
+			return new UserLoggedInEvent(state);
+		}
+	}
 }
